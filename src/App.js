dc9d364adf9023879c02f680baf1d87caa3b0202
Eva Lubomirova <44764379+evatsv@users.noreply.github.com>
2019-02-06 15:42:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+
 
 class App extends Component {
+  state = {
+  contacts: [
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "handle": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "rian",
+        "name": "Ryan Florence",
+        "handle": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+removeContact = (contact) => {
+this.setState((currentState) => ({
+contacts: currentState.contacts.filter((c) => {
+return c.id !== contact.id
+})
+}))
+  }
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts 
+        contacts={this.state.contacts} 
+        onDeleteContact= {this.removeContact}
+        />
       </div>
     );
   }
