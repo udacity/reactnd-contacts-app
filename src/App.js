1d099e57061a7e4b14b96a4e6d37d8540155c7e9
Mert Demirok <mertdemirok80@gmail.com>
2020-05-02 20:23:37 +0200

    enable delete button to delete contacts

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts'
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
 class App extends Component {
   state = {
-    contacts : [
+    contacts: [
       {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: '@richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg'
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "@richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
       },
       {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: '@karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg'
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "@karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
       },
       {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: '@tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-      }
-    ]
-  }
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "@tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((item) => {
+        return item.id !== contact.id;
+      }),
+    }));
+  };
 
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts} />
+        <ListContacts
+          deleteContact={this.removeContact}
+          contacts={this.state.contacts}
+        />
       </div>
     );
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -13,7 +13,7 @@ function ListContacts(props) {
             <p>{contact.name}</p>
             <p>{contact.handle}</p>
           </div>
-          <button className="contact-remove">Remove</button>
+          <button onClick={()=>props.deleteContact(contact)} className="contact-remove">Remove</button>
         </li>
       ))}
     </ol>
