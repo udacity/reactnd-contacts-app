542aea5c2e0078ab75dd3c96bcb8ffffdefba8d1
Mert Demirok <mertdemirok80@gmail.com>
2020-05-02 15:00:54 +0200

    add contact array into component state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: '@richardkalehoff',
-    avatarURL: 'http://localhost:5001/richard.jpg'
-  },
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: '@karen_isgrigg',
-    avatarURL: 'http://localhost:5001/karen.jpg'
-  },
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: '@tylermcginnis',
-    avatarURL: 'http://localhost:5001/tyler.jpg'
-  }
-]
 class App extends Component {
+  state = {
+    contacts : [
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
