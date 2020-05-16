cb523b748d136628b5824e3ef74747b1b9f230f9
Mert Demirok <mertdemirok80@gmail.com>
2020-05-16 23:35:30 +0200

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact);
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.concat([contact]),
+    }));
+  };
+
   render() {
     return (
       <div>
@@ -38,7 +45,17 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
