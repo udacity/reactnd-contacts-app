c4251a78090acc66c624239aeb582f38fe827140
jeraquis <jeraquis@users.noreply.github.com>
2020-06-05 02:36:19 -0700

    feat: ListContacts connection
    connected ListContacts to the app

---
+++ src/ListContacts.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class ListContacts extends Component {
+    render() {
+        console.log('Props', this.props)
+        return(
+            <ol className='contact-list'>
+                
+            </ol>
+        )
+    }
+}
+
+
+export default ListContacts
