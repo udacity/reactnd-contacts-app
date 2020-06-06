a69bd7e8f0da90f40e1496a878778865a32932a6
jeraquis <jeraquis@users.noreply.github.com>
2020-06-05 20:25:57 -0700

    feat: delete contact
    added functionality to delete contacts in the list using a button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,31 @@
 import React, {Component} from 'react'
 
-class ListContacts extends Component {
-    render() {
-        console.log('Props', this.props)
-        return(
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div 
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
+function ListContacts (props) {
 
+    return(
+        <ol className='contact-list'>
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div 
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button 
+                        className='contact-remove'
+                        onClick={() => props.onDeleteContact(contact)}    
+                    >
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
+}
 
 export default ListContacts
