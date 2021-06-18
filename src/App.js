dc08db2f1e3a52bffb0566e2666a2929ab52e52c
Khaled Adad <khaledadad@lewisu.edu>
2021-06-18 12:41:08 -0500

    changed names to match data coming from server

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import ListContacts from './ListContacts'
 
 const contacts = [
  {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
+   "id": "michael",
+   "name": "Michael Isgrigg",
+   "handle": "@michael_isgrigg",
+   "avatarURL": "http://localhost:5001/michael.jpg"
  },
  {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
+   "id": "ryan",
+   "name": "Ryan Kalehoff",
+   "handle": "@ryankalehoff",
+   "avatarURL": "http://localhost:5001/ryan.jpg"
  },
  {
    "id": "tyler",
    "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
+   "handle": "@tylermcginnis",
    "avatarURL": "http://localhost:5001/tyler.jpg"
  }
 ];
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,15 +2,27 @@ import React from 'react'
 
 const ListContacts = (props) => {
     return (
-      <>
-        {props.contacts.map( contact => {
-            return (
-              <div key={contact.id}>
-                <img src={contact.avatarURL} alt=''/>
-              </div>
+      <ol className='contacts-list'>
+        {props.contacts.map( contact => (
+            (
+              <li key={contact.id} className='contact-list-item'> 
+                <div
+                  className='contact-avatar'
+                  style={{ //first bracket says lets go into JS, 2nd bracket makes it an object
+                    backgroundImage: `url(${contact.avatarURL})`
+                  }}
+                ></div>
+                <div className='contact-details'>
+                  <p>{contact.name}</p>
+                  <p>{contact.handle}</p>
+                </div>
+                <button className='contact-remove'>
+                  Remove
+                </button>
+              </li>
             )
-          })}
-        </>
+        ))}
+        </ol>
     )
 }
 
