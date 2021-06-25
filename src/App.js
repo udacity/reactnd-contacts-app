2e78826f52fe417cb70650a28eee58a4007aaa94
Khaled Adad <khaledadad@lewisu.edu>
2021-06-25 10:43:42 -0500

    using state to hold contact array info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "michael",
-   "name": "Michael Isgrigg",
-   "handle": "@michael_isgrigg",
-   "avatarURL": "http://localhost:5001/michael.jpg"
- },
- {
-   "id": "ryan",
-   "name": "Ryan Kalehoff",
-   "handle": "@ryankalehoff",
-   "avatarURL": "http://localhost:5001/ryan.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "@tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
+const contacts = 
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "michael",
+        "name": "Michael Isgrigg",
+        "handle": "@michael_isgrigg",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "ryan",
+        "name": "Ryan Kalehoff",
+        "handle": "@ryankalehoff",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "@tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+];
+  }
   render() {
     return (
       <div>
