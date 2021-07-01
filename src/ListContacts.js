323491e619a5b8c31e6a99cbea6e7f0046bf717b
Khaled Adad <khaledadad@lewisu.edu>
2021-07-01 15:53:24 -0500

    added input field to LC comp and updateQuery fn

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,98 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-const ListContacts = (props) => {
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState(()=> ({
+      query: query.trim()
+    }))
+  }
+
+  render() {
     return (
-      <ol className='contacts-list'>
-        {props.contacts.map( contact => (
-            (
-              <li key={contact.id} className='contact-list-item'> 
-                <div
-                  className='contact-avatar'
-                  style={{ //first bracket says lets go into JS, 2nd bracket makes it an object
-                    backgroundImage: `url(${contact.avatarURL})`
-                  }}
-                ></div>
-                <div className='contact-details'>
-                  <p>{contact.name}</p>
-                  <p>{contact.handle}</p>
-                </div>
-                <button 
-                  onClick={() => props.onDeleteContact(contact)}
-                  className='contact-remove'>
-                  Remove
-                </button>
-              </li>
-            )
-        ))}
-        </ol>
+      <div className = 'list-contacts'>
+        {JSON.stringify(this.state)}
+        <div className= 'list-contacts-top'>
+          <input 
+            className='search-contacts'
+            type="text" 
+            placeholder='Search Contacts'
+            value={this.state.query}
+            onChange={(event)=> {this.updateQuery(event.target.value)}}
+            // To recap how user input affects the ListContacts component's own state:
+            // 1. The user enters text into the input field.
+            // 2. The onChange event listener invokes the updateQuery() function.
+            // 3. updateQuery() then calls setState(), merging in the new state to update the component's internal state.
+            // 4. Because its state has changed, the ListContacts component re-renders.            
+          />
+        </div>
+        <ol className='contacts-list'>
+          {this.props.contacts.map( contact => (
+              (
+                <li key={contact.id} className='contact-list-item'> 
+                  <div
+                    className='contact-avatar'
+                    style={{ //first bracket says lets go into JS, 2nd bracket makes it an object
+                      backgroundImage: `url(${contact.avatarURL})`
+                    }}
+                  ></div>
+                  <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.handle}</p>
+                  </div>
+                  <button 
+                    onClick={() => this.props.onDeleteContact(contact)}
+                    className='contact-remove'>
+                    Remove
+                  </button>
+                </li>
+              )
+          ))}
+          </ol>
+      </div>
     )
+  }
 }
+// const ListContacts = (props) => {
+//     return (
+//       <ol className='contacts-list'>
+//         {props.contacts.map( contact => (
+//             (
+//               <li key={contact.id} className='contact-list-item'> 
+//                 <div
+//                   className='contact-avatar'
+//                   style={{ //first bracket says lets go into JS, 2nd bracket makes it an object
+//                     backgroundImage: `url(${contact.avatarURL})`
+//                   }}
+//                 ></div>
+//                 <div className='contact-details'>
+//                   <p>{contact.name}</p>
+//                   <p>{contact.handle}</p>
+//                 </div>
+//                 <button 
+//                   onClick={() => props.onDeleteContact(contact)}
+//                   className='contact-remove'>
+//                   Remove
+//                 </button>
+//               </li>
+//             )
+//         ))}
+//         </ol>
+//     )
+// }
+
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContact: PropTypes.func.isRequired
+// }
 
 export default ListContacts
