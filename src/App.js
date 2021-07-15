b4ab0a93c34a8c70d914533ee49d0cbb39ecb40f
Khaled Adad <khaledadad@lewisu.edu>
2021-07-15 12:15:41 -0500

    using ContactsAPI to fetch and remove

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+// contacts: [
+//       {
+//         "id": "michael",
+//         "name": "Michael Isgrigg",
+//         "handle": "@michael_isgrigg",
+//         "avatarURL": "http://localhost:5001/michael.jpg"
+//       },
+//       {
+//         "id": "ryan",
+//         "name": "Ryan Kalehoff",
+//         "handle": "@ryankalehoff",
+//         "avatarURL": "http://localhost:5001/ryan.jpg"
+//       },
+//       {
+//         "id": "tyler",
+//         "name": "Tyler McGinnis",
+//         "handle": "@tylermcginnis",
+//         "avatarURL": "http://localhost:5001/tyler.jpg"
+//       },
+//     ]
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "michael",
-        "name": "Michael Isgrigg",
-        "handle": "@michael_isgrigg",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "ryan",
-        "name": "Ryan Kalehoff",
-        "handle": "@ryankalehoff",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "@tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      },
-    ]
+    contacts: []
   }
 
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
   removeContact = contact => {
     this.setState((currentState) => ({ 
       contacts: currentState.contacts.filter((c) => {
         return c.id !== contact.id
       })
     }))
+    ContactsAPI.remove(contact)
   }
   render() {
     return (
